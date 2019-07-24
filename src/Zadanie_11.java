2396b4db416e85b9e3ef7072ea94cfdd4e71dded
TesterIashka <test.space.job@gmail.com>
2019-07-24 14:45:02 +0300

    Задание 11

---
+++ src/Zadanie_11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Zadanie_11
+{
+    public static void main (String args[])
+    {
+        Scanner joke = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String anek = joke.nextLine();
+        System.out.println("Ваша строка: "+anek);
+    }
+}
